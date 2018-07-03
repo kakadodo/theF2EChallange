4c2a240b742552a0ba1de33ce951eeac0b97a628
Chelsea <chelsea@creation.com.tw>
2018-07-03 16:21:03 +0800

    week 5

---
+++ source/js/comic-viewer.js
@@ -0,0 +1,93 @@
+$(function(){
+  //perfect scroller
+  const ps = new PerfectScrollbar('.comic_page_viewer_nav');
+  // toogle to comic page
+  const chapterBtn = $('.comic_chapters_lists a');
+  chapterBtn.click(function(e){
+    e.preventDefault();
+    $('.comic_index').addClass('d-none');
+    $('.comic_page').removeClass('d-none');
+    window.scrollTo(0,0);
+  });
+  //toogle to comic index
+  $('.comic_page_title').click(function(e){
+    e.preventDefault();
+    $('.comic_index').removeClass('d-none');
+    $('.comic_page').addClass('d-none');
+    window.scrollTo(0,0);
+  });
+  //toogle view mode
+  $('#view_mode').click(function(){
+    $('#app').toggleClass('dark');
+  });
+  //count page
+  const pages = {
+    currentPage:1,
+    totalPage:12,
+    currentPageUrl(){
+      return `https://hexschool.github.io/THE_F2E_Design/week5-comic%20viewer/assets/storyboard-${this.currentPage}.png`
+    },
+    checkViewerBtn(){
+      $('.viewer_btn').removeAttr('disabled');
+      if(this.currentPage==1){
+        $('.viewer_btn.left').attr('disabled',true);
+      }else if(this.currentPage == this.totalPage){
+        $('.viewer_btn.right').attr('disabled',true);
+      }
+    },
+    updateSelectChapter(){
+      $('.select_page option').removeAttr('selected');
+      $(`.select_page option[value="p-${this.currentPage}"]`).attr('selected',true);
+    }
+  }
+  
+  const viewerImg = $('.comic_page_viewer img')[0];
+  //page nav click
+  $('.page').click(function(){
+    $('.page').removeClass('active');
+    $(this).addClass('active');
+    pages.currentPage = $(this).data('page');
+    const navWidth = $('.comic_page_viewer_nav')[0].offsetWidth;
+    if($(this)[0].offsetLeft >= navWidth){
+      $('.comic_page_viewer_nav')[0].scrollLeft = $(this)[0].offsetLeft - navWidth + 80;
+    }else{
+      $('.comic_page_viewer_nav')[0].scrollLeft = 0;
+    }
+    viewerImg.src=pages.currentPageUrl();
+    pages.checkViewerBtn();
+    pages.updateSelectChapter();
+  });
+  //page nav scroll btn
+  $('.viewer_nav_btn').click(function(){
+    const navWidth = $('.comic_page_viewer_nav')[0].offsetWidth;
+    if($(this).is('.left')){
+      $('.comic_page_viewer_nav')[0].scrollLeft = 0;
+    }else{
+      $('.comic_page_viewer_nav')[0].scrollLeft = $('.comic_page_viewer_nav')[0].scrollWidth - navWidth;
+    }
+  });
+  //viewer btn
+  $('.viewer_btn').click(function(){
+    if($(this).is('.left')){
+      pages.currentPage--;
+    }else{
+      pages.currentPage++;
+    }
+    $(`.page[data-page="${pages.currentPage}"]`).click();
+    pages.checkViewerBtn();
+  });
+  //page select
+  $('.select_page').change(function(){
+    const page = $(this).val().split('-')[1];
+    pages.currentPage = parseInt(page);
+    $(`.page[data-page="${pages.currentPage}"]`).click();
+  });
+  //open fullscreen
+  $('.comic_page_viewer img').click(function(){
+    $(this).parent().addClass('fullscreen');
+  });
+  //close fullscreen
+  $('.close_btn').click(function(){
+    $(this).closest('.comic_page_viewer').removeClass('fullscreen');
+  });
+});

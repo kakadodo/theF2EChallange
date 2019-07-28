44795d9812da80e5333f44befb635d1d6b1acbdc
Chelsea Chang <gogoieshdr@gmail.com>
2019-07-28 20:42:39 +0800

    Update 2019-07-28T12:42:35.905Z

---
+++ js/mp3player.js
@@ -0,0 +1,260 @@
+'use strict';
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var player = null;
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('js_player', {
+    height: '0',
+    width: '0',
+    videoId: 'lzkKzZmRZk8',
+    events: {
+      'onStateChange': vm.onPlayerStateChange
+    }
+  });
+}
+
+var vm = null;
+
+$(function () {
+  vm = new Vue({
+    el: '#app',
+    data: {
+      slideMenu: true,
+      showList: true,
+      playlist: [],
+      ads: [],
+      activePlaylistIndex: 0,
+      activeAdIndex: 0,
+      menuList: {},
+      currentPlaylist: {},
+      currentSongIndex: 0,
+      currentSongTime: 0,
+      songTimer: null,
+      isRandom: false,
+      isPlay: false,
+      volumn: 0.5,
+      isSlide: false,
+      pos: {
+        originX: 0,
+        moveX: 0,
+        innerWidth: 0,
+        outerWidth: 0
+      }
+    },
+    computed: {
+      currentSong: function currentSong() {
+        if (this.currentPlaylist.list) {
+          return this.currentPlaylist.list[this.currentSongIndex];
+        }
+        return '';
+      },
+      displayTimeBar: function displayTimeBar() {
+        if (this.currentPlaylist.list) {
+          var currentSongTotalTime = this.currentPlaylist.list[this.currentSongIndex].list_duration;
+          var percent = this.currentSongTime / currentSongTotalTime * 100;
+          return {
+            width: percent + '%'
+          };
+        }
+        return '';
+      },
+      displayVolBar: function displayVolBar() {
+        var percent = this.volumn / 1 * 100;
+        return {
+          width: percent + '%'
+        };
+      }
+    },
+    methods: {
+      toggleList: function toggleList(e) {
+        this.showList ? $(e.target).next().slideUp('fast') : $(e.target).next().slideDown('fast');
+        this.showList = !this.showList;
+      },
+      showMenuList: function showMenuList(cataType, payload) {
+        var tempArr = [];
+        switch (cataType) {
+          case 1:
+            this.activePlaylistIndex = payload;
+            this.menuList = this.playlist[payload];
+            break;
+          case 2:
+            this.playlist.forEach(function (list) {
+              tempArr = [].concat(_toConsumableArray(tempArr), _toConsumableArray(list.list));
+            });
+            tempArr.sort(function (a, b) {
+              var aa = a['artist_name'].toLowerCase().trim();
+              var bb = b['artist_name'].toLowerCase().trim();
+              return aa === bb ? 0 : aa < bb ? -1 : 1;
+            });
+            this.menuList = {
+              title: '歌手',
+              list: tempArr
+            };
+            this.activePlaylistIndex = null;
+            break;
+          case 3:
+            this.playlist.forEach(function (list) {
+              tempArr = [].concat(_toConsumableArray(tempArr), _toConsumableArray(list.list.filter(function (l) {
+                return l.favorite;
+              })));
+            });
+            this.menuList = {
+              title: '我的最愛',
+              list: tempArr
+            };
+            this.activePlaylistIndex = null;
+            break;
+          default:
+            break;
+        }
+      },
+      onPlayerStateChange: function onPlayerStateChange(e) {
+        if (e.data === YT.PlayerState.ENDED) {
+          this.playSong(1);
+        }
+      },
+      playSong: function playSong(step, from) {
+        this.currentPlaylist = this.menuList;
+        // 從播放清單選取
+        if (from === 'list') {
+          this.currentSongIndex = step;
+          this.callPlayer();
+          return;
+        }
+        // 隨機播放清單歌曲
+        if (this.isRandom) {
+          var randomIndex = this.getRandomNum(this.currentPlaylist.list.length);
+          if (randomIndex === this.currentSongIndex) {
+            randomIndex = this.getRandomNum(this.currentPlaylist.list.length);
+          }
+          this.currentSongIndex = randomIndex;
+          this.callPlayer();
+          return;
+        }
+        // 依照順序播放上下首
+        var tempSong = this.currentPlaylist.list[this.currentSongIndex + step];
+        if (!tempSong) {
+          if (step === 1) {
+            this.currentSongIndex = 0;
+          } else {
+            this.currentSongIndex = this.currentPlaylist.list.length - 1;
+          }
+        } else {
+          this.currentSongIndex += step;
+        }
+        this.callPlayer();
+      },
+      callPlayer: function callPlayer() {
+        // 讀取 youtube 音源
+        if (this.isPlay) {
+          player.loadVideoById({
+            videoId: this.currentPlaylist.list[this.currentSongIndex].id
+          });
+          this.setSongTimer();
+        } else {
+          player.cueVideoById({
+            videoId: this.currentPlaylist.list[this.currentSongIndex].id
+          });
+        }
+      },
+      changeSongState: function changeSongState(boolean) {
+        this.isPlay = boolean;
+        player.setVolume(this.volumn * 100);
+        if (this.isPlay) {
+          player.playVideo();
+          this.setSongTimer();
+        } else {
+          player.pauseVideo();
+          clearInterval(this.songTimer);
+        }
+      },
+      getRandomNum: function getRandomNum(length) {
+        return Math.floor(Math.random() * length);
+      },
+      setSongTimer: function setSongTimer() {
+        var _this = this;
+
+        this.songTimer = setInterval(function () {
+          if (!_this.isSlide) {
+            _this.currentSongTime = Math.floor(player.getCurrentTime());
+          }
+        }, 1000);
+      },
+      switchAds: function switchAds() {
+        var _this2 = this;
+
+        $('.ads').css({
+          display: 'none'
+        });
+        if (this.activeAdIndex === this.ads.length) {
+          this.activeAdIndex = 0;
+        }
+        $('.ads').eq(this.activeAdIndex).css({
+          display: 'block'
+        });
+        this.activeAdIndex++;
+        setTimeout(function () {
+          _this2.switchAds();
+        }, 5000);
+      },
+      onmouseDown: function onmouseDown(e) {
+        this.isSlide = true;
+        this.pos.originX = e.x;
+        this.pos.innerWidth = $(e.target).parent().width();
+        this.pos.outerWidth = $(e.target).parent().parent().width();
+      },
+      onmouseMove: function onmouseMove(e) {
+        if (this.isSlide) {
+          this.pos.moveX = e.x;
+          var distance = this.pos.moveX - this.pos.originX;
+          var totalWidth = this.pos.innerWidth + distance;
+          if (totalWidth >= this.pos.outerWidth) {
+            totalWidth = this.pos.outerWidth;
+          } else if (totalWidth < 0) {
+            totalWidth = 0;
+          }
+          $(e.target).parent().width(totalWidth);
+        }
+      },
+      onmouseUp: function onmouseUp(e, type) {
+        this.isSlide = false;
+        this.pos.innerWidth = $(e.target).parent().width();
+        var percent = (this.pos.innerWidth / this.pos.outerWidth).toFixed(1);
+        if (type === 'vol') {
+          this.volumn = +percent;
+          player.setVolume(this.volumn * 100);
+        } else if (type === 'time') {
+          this.currentSongTime = Math.floor(this.currentPlaylist.list[this.currentSongIndex].list_duration * percent);
+          player.seekTo(this.currentSongTime, true);
+        }
+      }
+    },
+    filters: {
+      formatTime: function formatTime(val) {
+        var minute = '00' + Math.floor(val / 60);
+        var second = '00' + val % 60;
+        return minute.substr(-2) + ':' + second.substr(-2);
+      }
+    },
+    created: function created() {
+      var _this3 = this;
+
+      $.getJSON('../json/mp3player.json', function (res) {
+        _this3.playlist = res.playlist;
+        _this3.ads = res.ads;
+        _this3.menuList = _this3.playlist[_this3.activePlaylistIndex];
+        _this3.currentPlaylist = _this3.menuList;
+      });
+    },
+    mounted: function mounted() {
+      this.switchAds();
+    }
+  });
+});
+//# sourceMappingURL=mp3player.js.map

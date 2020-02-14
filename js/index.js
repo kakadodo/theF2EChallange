7a6f44b2c850d26fd6b24d6fe81936e3febb4c32
Chelsea Chang <gogoieshdr@gmail.com>
2020-02-14 20:09:45 +0800

    Update 2020-02-14T12:09:42.473Z

--- js/index.js
+++ js/index.js
@@ -1,15 +1,2 @@
-'use strict';
-
-$(function () {
-  $.get('./json/item-list.json', function (data) {
-    $('.js_loading').hide();
-    var temp = '';
-    data.forEach(function (d) {
-      temp += '\n        <div class="card">\n          <div class="card-img">\n            <img src="' + d.imgSrc + '" alt="' + d.title + '" class="card-img-top">\n          </div>\n          <div class="card-body">\n            <h5 class="card-title">' + d.title + '</h5>\n            <p class="card-text">\n              Skill Point:\n              ' + d.skills.map(function (skill) {
-        return '\n                <span class="badge badge-warning mr-1">#' + skill + '</span>\n              ';
-      }).join("") + '\n            </p>\n          </div>\n          <div class="card-footer d-flex justify-content-between">\n            <small class="text-muted">' + d.date + '</small>\n            <span class="badge badge-pill badge-dark">' + d.edition + '</span>\n          </div>\n          <div class="card_cover">\n            <a href="' + d.href + '" target="_blank">\n              <button class="btn btn-outline-goblue btn-lg">Checkout</button>\n            </a>\n          </div>\n        </div>\n      ';
-    });
-    $('.js_itemList').html(temp);
-  });
-});
+"use strict";$(function(){$.get("./json/item-list.json",function(n){$(".js_loading").hide();var t="";n.forEach(function(n){t+='\n        <div class="card">\n          <div class="card-img">\n            <img src="'+n.imgSrc+'" alt="'+n.title+'" class="card-img-top">\n          </div>\n          <div class="card-body">\n            <h5 class="card-title">'+n.title+'</h5>\n            <p class="card-text">\n              Skill Point:\n              '+n.skills.map(function(n){return'\n                <span class="badge badge-warning mr-1">#'+n+"</span>\n              "}).join("")+'\n            </p>\n          </div>\n          <div class="card-footer d-flex justify-content-between">\n            <small class="text-muted">'+n.date+'</small>\n            <span class="badge badge-pill badge-dark">'+n.edition+'</span>\n          </div>\n          <div class="card_cover">\n            <a href="'+n.href+'" target="_blank">\n              <button class="btn btn-outline-goblue btn-lg">Checkout</button>\n            </a>\n          </div>\n        </div>\n      '}),$(".js_itemList").html(t)})});
 //# sourceMappingURL=index.js.map
--- js/product-gallery.js
+++ js/product-gallery.js
@@ -1,27 +1,2 @@
-'use strict';
-
-var imgs = Array.from(document.querySelectorAll('.product img'));
-var indexContainer = document.querySelector('.index');
-var pd_pageContainer = document.querySelector('.pd_page');
-var backBtn = document.querySelector('.back');
-imgs.forEach(function (img) {
-  img.addEventListener('click', function () {
-    indexContainer.classList.remove('active');
-    pd_pageContainer.classList.add('active');
-    smoothscroll();
-  });
-});
-backBtn.addEventListener('click', function (e) {
-  e.preventDefault();
-  pd_pageContainer.classList.remove('active');
-  indexContainer.classList.add('active');
-});
-
-function smoothscroll() {
-  var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
-  if (currentScroll > 0) {
-    window.requestAnimationFrame(smoothscroll);
-    window.scrollTo(0, currentScroll - currentScroll / 5);
-  }
-}
+"use strict";var imgs=Array.from(document.querySelectorAll(".product img")),indexContainer=document.querySelector(".index"),pd_pageContainer=document.querySelector(".pd_page"),backBtn=document.querySelector(".back");function smoothscroll(){var e=document.documentElement.scrollTop||document.body.scrollTop;0<e&&(window.requestAnimationFrame(smoothscroll),window.scrollTo(0,e-e/5))}imgs.forEach(function(e){e.addEventListener("click",function(){indexContainer.classList.remove("active"),pd_pageContainer.classList.add("active"),smoothscroll()})}),backBtn.addEventListener("click",function(e){e.preventDefault(),pd_pageContainer.classList.remove("active"),indexContainer.classList.add("active")});
 //# sourceMappingURL=product-gallery.js.map
